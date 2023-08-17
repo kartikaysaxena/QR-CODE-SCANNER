ac2cd2f516340976efcf3f7f7e9b23a4899bf93a
Kartikay <kartikay_2101ce32@iitp.ac.in>
2023-08-17 20:53:25 +0530

    first
    Signed-off-by: Kartikay <kartikay_2101ce32@iitp.ac.in>

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import Html5QrcodePlugin from './Main';
+import zlib from 'react-zlib-js'
+import {Buffer} from 'buffer';
+function App() {
+  const onNewScanResult = (decodedText, decodedResult) => {
+
+    console.log(decodedText)
+    const compressedData = decodedText;
+    const buffer = Buffer.from(compressedData, 'base64');
+    const decompressedData = zlib.inflateSync(buffer).toString('utf8');
+    console.log(JSON.parse(decompressedData))
+};
+
+  return (
+    <div className="App">
+            <Html5QrcodePlugin
+                fps={10}
+                qrbox={800}
+                disableFlip={false}
+                qrCodeSuccessCallback={onNewScanResult}
+            />
+        </div>
+  );
+}
+
+export default App;
